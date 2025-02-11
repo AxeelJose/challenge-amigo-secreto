5773e871d3a5474228ec65f5618b5ae4b9666de5
AxeelJose <axeeljose@gmail.com>
2025-02-11 16:12:36 -0500

    función agregar amigos
    Se agregó la función agregarAmigo() y la función limpiarCaja()

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    //Validar entrada
+    if (nombreAmigo.trim() === ''){
+        limpiarCaja();
+        alert ('Por favor, inserte un nombre.');
+    } else {
+        amigos.push(nombreAmigo);
+        console.log(amigos);
+        limpiarCaja();
+    }
+}
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
