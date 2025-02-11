7d2fd11ecea4071a0056cf830f2b1268d839d847
AxeelJose <axeeljose@gmail.com>
2025-02-11 17:11:27 -0500

    corrección de código
    Se eliminaron los console.log() del código

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -9,7 +9,6 @@ function agregarAmigo() {
         alert('Por favor, inserte un nombre.');
     } else {
         amigos.push(nombreAmigo);
-        console.log(amigos);
         limpiarCaja();
         agregarLista();
     }
@@ -19,9 +18,17 @@ function limpiarCaja() {
     document.getElementById('amigo').value = '';
 }
 
-function agregarLista() {
+function limpiarLista() {
     let listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = '';
+}
+
+function vaciarLista(){
+    amigos.length = 0;
+}
+
+function agregarLista() {
+    limpiarLista();
 
     for (let i = 0; i < amigos.length; i++) {
         let li = document.createElement('li');
@@ -32,14 +39,14 @@ function agregarLista() {
 
 function sortearAmigo() {
     let numeroAmigos = amigos.length;
-    console.log(numeroAmigos);
     if (amigos.length === 0) {
         alert('La lista de amigos está vacía.');
     } else {
         let indice = Math.floor(Math.random() * numeroAmigos);
-        console.log(indice);
         let amigoSeleccionado = amigos[indice];
         let casillaResultado = document.getElementById('resultado');
+        limpiarLista();
         casillaResultado.innerHTML = 'El amigo sorteado es: ' + amigoSeleccionado;
+        vaciarLista();
     }
 }
